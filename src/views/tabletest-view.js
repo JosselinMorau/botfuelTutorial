7e929085b1bdb5a930f95cde6928056affb2e2ba
JosselinMorau <moraujosse@eisti.eu>
2018-06-25 14:14:18 +0200

    table5

--- src/views/tabletest-view.js
+++ src/views/tabletest-view.js
@@ -4,7 +4,7 @@ class TabletestView extends View {
   render() {
     const data = {
       schema: [{ name: 'Name' }, { city: 'City' }],
-      rows: [{ name: 'John', city: 'New York' }, { name: 'Peter', city: 'Paris' }]
+      rows: [{ name: 'John', city: 'New York' }, { name: 'Peter', city: 'Paris' }],
     };
     return [new BotTableMessage(data)];
 
