2ce43fc2b3a54f6635dabdf85a59d246e0dd953b
JosselinMorau <moraujosse@eisti.eu>
2018-06-25 14:05:39 +0200

    table4

--- src/views/tabletest-view.js
+++ src/views/tabletest-view.js
@@ -2,9 +2,9 @@ const { View, BotTableMessage } = require('botfuel-dialog');
 
 class TabletestView extends View {
   render() {
-    const data = { schema: [{ name: 'Name' }, { city: 'City' }],
-      rows: [{ name: 'John', city: 'New York' },
-    { name: 'Peter', city: 'Paris' }]
+    const data = {
+      schema: [{ name: 'Name' }, { city: 'City' }],
+      rows: [{ name: 'John', city: 'New York' }, { name: 'Peter', city: 'Paris' }]
     };
     return [new BotTableMessage(data)];
 
