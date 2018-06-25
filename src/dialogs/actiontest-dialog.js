134967772d7a17a12756a18e481d2fe201990dab
JosselinMorau <moraujosse@eisti.eu>
2018-06-25 12:10:09 +0200

    action

---
+++ src/dialogs/actiontest-dialog.js
@@ -0,0 +1,5 @@
+const { BaseDialog } = require('botfuel-dialog');
+
+class Actiontest extends BaseDialog {}
+
+module.exports = Actiontest;
---
+++ src/views/actiontest-view.js
@@ -0,0 +1,17 @@
+const { View, ActionsMessage, Link, Postback } = require('botfuel-dialog');
+
+class ActiontestView extends View {
+  render() {
+
+    const actions = [
+      new Link('Botfuel', 'https://www.botfuel.io'),
+      new Postback('Hello', 'greetings', []),
+    ];
+
+    return [new ActionsMessage(actions)];
+
+  }
+}
+
+module.exports = ActiontestView;
+
