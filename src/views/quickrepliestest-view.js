1ec01d25cc68752f5e5023fdf72f9ace69f785fa
JosselinMorau <moraujosse@eisti.eu>
2018-06-25 11:48:26 +0200

    quickreplies4

--- src/views/quickrepliestest-view.js
+++ src/views/quickrepliestest-view.js
@@ -3,7 +3,7 @@ const { View, QuickrepliesMessage } = require('botfuel-dialog');
 class QuickrepliestestView extends View {
   render() {
 
-    return [new QuickrepliesMessage(['Paris','London'],'Where do you want to go?')];
+    return [new QuickrepliesMessage(['Paris','London'],{ text: 'Where do you want to go?' })];
 
   }
 }
